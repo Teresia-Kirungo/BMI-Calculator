daee09d95b2d5c63c241e8389d12fdb34a03d336
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2020-12-25 20:38:31 +0300

    Update bm1.js

--- bm1.js
+++ bm1.js
@@ -1,10 +1,25 @@
 //Create your function below this line.
 function bmiCalculator(weight, height){
+  //The first parameter should be the weight and the second should be the height. 
+
+  /* The Math.pow() function returns the base to the exponent power, that is, base exponent.
+  
+  console.log(Math.pow(4, 3)); //this is 4 X 4 X 4
+// expected output: 64
+
+  */
+
     var bmi = weight/(Math.pow(height,2));
     return bmi;
 }
-//The first parameter should be the weight and the second should be the height.
-bmiCalculator(65, 1.8); 
+
+Math.round(bmi = bmiCalculator(65, 1.8));
+/* The Math.round() function returns the value of a number rounded to the nearest integer.
+
+console.log(Math.round(0.9));
+// expected output: 1
+*/
+
 
 /* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
 
